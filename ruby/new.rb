acc792e305ddb7f3b15c1a0e796b97fc716aa5cb
Yuya0829 <yuya.0829fantasista@gmail.com>
2022-10-27 07:33:07 +0000

    Ruby 9 fin

--- ruby/new.rb
+++ ruby/new.rb
@@ -1,6 +1,12 @@
 class Car
+
+  def move(direction, distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+
   def turn(direction)
-    puts "#{direction}に曲がります"
+    puts "#{direction}に曲がります。"
   end
 
   def run(distance)
@@ -9,7 +15,12 @@ class Car
 end
 
 car = Car.new
-car.turn("右")
+car.move("右", 5)
 
-car = Car.new
-car.run(5)
+class Car
+  def self.run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+Car.run(10)
